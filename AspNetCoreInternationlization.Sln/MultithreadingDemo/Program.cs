d5181da66dc995abb689a70a1209ebb452646fa8
parvizrovshanaliyev <parvizra@code.edu.az>
2020-03-04 11:09:18 +0400

    .....

---
+++ AspNetCoreInternationlization.Sln/MultithreadingDemo/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MultithreadingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = new CultureInfo("pt-BR");
+            //
+            ThreadInfo();
+            //
+            TaskFactory tf = new TaskFactory();
+            tf.StartNew(BackgroundTask);
+            Task.Delay(3000).Wait();
+            ///////
+            CultureInfo.CurrentCulture = new CultureInfo("ru-RU");
+            ///////
+            ThreadInfo();
+            Task.Delay(3000).Wait();
+
+        }
+
+        static void ThreadInfo()
+        {
+            Console.WriteLine($"{Thread.CurrentThread.ManagedThreadId}:{CultureInfo.CurrentCulture.DisplayName}");
+        }
+
+        static void BackgroundTask()
+        {
+            while (true)
+            {
+                ThreadInfo();
+                Task.Delay(1000).Wait();
+            }
+        }
+    }
+}
