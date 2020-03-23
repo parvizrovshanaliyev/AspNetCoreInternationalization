490b437c5963dcfef2978eabef7bc6891aa95053
parvizrovshanaliyev <parvizra@code.edu.az>
2020-03-23 16:44:22 +0400

    set ui culture loading

---
+++ AspNetCoreInternationlization.Sln/RequestLocalizationDemo/Controllers/EnumerationsController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using RequestLocalizationDemo.Enumerations;
+using RequestLocalizationDemo.Models;
+
+namespace RequestLocalizationDemo.Controllers
+{
+    public class EnumerationsController : Controller
+    {
+        private IStringLocalizer<Gender> _genderLocalizer;
+
+        public EnumerationsController(IStringLocalizer<Gender> genderLocalizer)
+        {
+            _genderLocalizer = genderLocalizer;
+        }
+
+        public IActionResult Genders()
+        {
+            Console.WriteLine("+++++++++++=====+++++++++++");
+            Console.WriteLine($"Current Culture : {CultureInfo.CurrentCulture}");
+            Console.WriteLine($"Current UI Culture : {CultureInfo.CurrentUICulture}");
+            List<SelectItem> selectList =
+                new List<SelectItem>();
+            Array values = Enum.GetValues(typeof(Gender));
+
+            foreach (object value in values)
+            {
+                selectList.Add(new SelectItem
+                {
+
+                    //Name = Enum.GetName(typeof(Gender), value),
+                    // localization
+                    Name = _genderLocalizer[value.ToString()],
+                    Value = (int)value
+                });
+            }
+
+            return Ok(selectList);
+        }
+    }
+}
---
+++ AspNetCoreInternationlization.Sln/RequestLocalizationDemo/Startup.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Newtonsoft.Json.Serialization;
+
+namespace RequestLocalizationDemo
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // localization
+            services.AddLocalization(options =>
+            {
+                options.ResourcesPath="Resources";
+            });
+            //
+            services.Configure<RequestLocalizationOptions>(options =>
+            {
+                options.SupportedUICultures = new List<CultureInfo>
+                {
+                    new CultureInfo("bs"),
+                    new CultureInfo("de"),
+                    new CultureInfo("es"),
+                    new CultureInfo("fr-FR"),
+                    new CultureInfo("en-GB")
+                };
+
+                // default culture
+                options.DefaultRequestCulture = new RequestCulture("es");
+            });
+            //
+            services.AddMvc(options =>
+            {
+                options.EnableEndpointRouting = false;
+                //
+                options.RespectBrowserAcceptHeader = true; // false by default
+                // xml
+                //options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+
+            });
+                
+            
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // request localization
+            app.UseRequestLocalization();
+            //
+            app.UseMvcWithDefaultRoute();
+        }
+    }
+}
