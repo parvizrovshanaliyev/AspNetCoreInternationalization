904f50f51f85c786d83adec70981adbd9e9995ab
parvizrovshanaliyev <parvizra@code.edu.az>
2020-03-23 16:48:26 +0400

    set ui cultures ready

---
+++ AspNetCoreInternationlization.Sln/RequestLocalizationDemo/Controllers/CulturesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RequestLocalizationDemo.Controllers
+{
+    public class CulturesController : Controller
+    {
+        public IActionResult Set(string uiCulture)
+        {
+            RequestCulture requestCulture =
+                new RequestCulture(uiCulture);
+
+            string coockieValue =
+                CookieRequestCultureProvider.MakeCookieValue(requestCulture);
+
+            Response.Cookies.Append(CookieRequestCultureProvider.DefaultCookieName,
+                coockieValue);
+            return Ok();
+        }
+    }
+}
